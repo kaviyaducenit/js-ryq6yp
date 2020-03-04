ad9d9f812cd3d6dcb9089eaac480d52f64700c64
kaviyaducenit <61681527+kaviyaducenit@users.noreply.github.com>
2020-03-04 17:56:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// var person=prompt("please Enter your name");
-// var start=prompt("Start index");
-// while(isNaN(start)==true)
-// {
-//   alert("Enter valid value");
-//   var start=prompt("start index");
-// }
-// var end=prompt("end index");  
-// while(isNaN(end)==true)
-// {
-//   alert("Enter valid value");
-//   var end=prompt("End index");
-// }
-// alert(person.slice(start,end));
+let person=prompt("please Enter your name");
+let start=prompt("Start index");
+ while(isNaN(start)==true)
+{
+  alert("Enter valid value");
+  let start=prompt("start index");
+}
+let end=prompt("end index");  
+while(isNaN(end)==true)
+{
+  alert("Enter valid value");
+  let end=prompt("End index");
+}
+alert(person.slice(start,end));
